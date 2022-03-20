1441655c8aeaaf1b08df97b8e4212e10b735eb86
Jason Adams <jmadams41@gmail.com>
2022-03-20 17:22:08 -0600

    first inquirer question done

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const inquirer = require("inquirer");
+const markDown = require('./utils/generateMarkdown.js');
+//const writeFile = require('./utils/fileGen.js');//
+
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Username',
+            message: 'What is your username on GitHub',
+            validate: descInput => {
+                if(descInput) {
+                    return true;
+                } else {
+                    console.log('please enter a valid GitHib username')
+                    return false;
+                }
+            }
+        }
+    ])
+}
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
